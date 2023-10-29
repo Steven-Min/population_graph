0d5bbd05b4cf9a214e5ffa1928a9e99619656c59
Steven-Min <itcloud02147@gmail.com>
2023-10-29 11:03:36 +0800

    APIから都道府県一覧取得

---
+++ src/const/index.ts
@@ -0,0 +1,2 @@
+export const RESAS_API_URL: string = `https://opendata.resas-portal.go.jp`
+export const API_KEY: string = `vqQnYCGTe75uDG9JEiLzuekj4JoBndW9jiyjl80q`
--- src/services/PopulationService.ts
+++ src/services/PopulationService.ts
@@ -1,8 +1,12 @@
+import axios from 'axios'
+import { RESAS_API_URL, API_KEY } from '../const'
+import type { PREFECTURE } from '../type'
+
 /**
  * 人口データを取得するサービス
  */
 export class PopulationService {
-  public prefectures: string[] = []
+  public prefectures: PREFECTURE[] = []
   public selectedPrefectures: string[] = []
   public graphData: any = {}
 
@@ -10,5 +14,12 @@ export class PopulationService {
     this._initData()
   }
 
-  private async _initData() {}
+  private async _initData() {
+    const res = await axios.get(`${RESAS_API_URL}/api/v1/prefectures`, {
+      headers: {
+        'X-API-KEY': API_KEY
+      }
+    })
+    this.prefectures = res.data.result
+  }
 }
