57feadc7bcf9b56428751ecf6109854103733bf8
Steven-Min <itcloud02147@gmail.com>
2023-11-14 12:43:54 +0800

    タイプ補足

--- src/type/index.ts
+++ src/type/index.ts
@@ -2,3 +2,14 @@ export type PREFECTURE = {
   prefCode: number
   prefName: string
 }
+
+export type POPULATION_DATA = {
+  year: number
+  value: number
+}
+
+export type GRAPH_DATASET = {
+  label: string
+  backgroundColor: string
+  data: number[]
+}
